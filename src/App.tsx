406ce387fddf502f5aaa0384ac173ad18b5dccf1
MohamedYassineYAHYAOUI <yahyaouiy1997@gmail.com>
2022-12-27 12:07:17 +0100

    tests library + react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div>works</div>;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App;
+export function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
